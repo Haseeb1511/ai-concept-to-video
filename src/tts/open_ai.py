5a3f88e02a1991034c86a743f5cc359a8f384fd6
haseeb <haseebmanzoor1511@gmail.com>
2026-04-02 21:42:55 +0500

    agent fixing pipline working

--- src/tts/open_ai.py
+++ src/tts/open_ai.py
@@ -11,7 +11,7 @@ async def text_to_speech_bytes(text: str) -> bytes:
     try:
         response = await client.audio.speech.create(
             model="tts-1",
-            voice="alloy",
+            voice="onyx",
             input=text
         )
         return response.content
@@ -23,3 +23,36 @@ async def text_to_speech_bytes(text: str) -> bytes:
 # tts-1
 # 1,000,000 characters → $15
 # $0.06 per 4096 characters of speech
+
+
+
+
+
+
+# to check differnt voice smaple
+# client = OpenAI()
+
+# voices = [
+#     "alloy","ash","ballad","coral","echo",
+#     "fable","nova","onyx","sage","shimmer",
+#     "verse","marin","cedar"
+# ]
+
+# text = "Hello, this is a test of OpenAI text to speech voices."
+
+# for v in voices:
+#     out_file = Path(f"{v}.mp3")
+    
+#     with client.audio.speech.with_streaming_response.create(
+#         model="gpt-4o-mini-tts",
+#         voice=v,
+#         input=text
+#     ) as response:
+#         response.stream_to_file(out_file)
+
+#     print(f"Generated voice: {v}")
+
+
+# onyx
+# fable
+# echo
